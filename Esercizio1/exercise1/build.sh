8a2d40ab7c56e8764bca0b77a93b5cf8ba5406fc
Gianfranco Duminuco <gianfranco.duminuco@gmail.com>
2023-03-24 17:19:31 +0100

    Inizio

--- Esercizio1/exercise1/build.sh
+++ Esercizio1/exercise1/build.sh
@@ -1,6 +1,6 @@
 
 #! /bin/bash
-//ciao
+
 g++ -O3 -std=c++20 -o main main.cpp \
   zmytest/test.cpp zlasdtest/test.cpp zlasdtest/container/container.cpp \
   zlasdtest/exercise1a/simpletest.cpp zlasdtest/exercise1a/fulltest.cpp \
