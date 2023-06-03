e6152025d3ebc952610aaa49d1a777f65568d2cf
Gianfranco Duminuco <gianfranco.duminuco@gmail.com>
2023-06-03 17:29:43 +0200

    FINE

--- Esercizio3/exercise3/build.sh
+++ Esercizio3/exercise3/build.sh
@@ -1,7 +1,7 @@
 
 #! /bin/bash
 
-g++ -O3 -std=c++20 -o main main.cpp \
+g++ -O3 -std=c++20 -fsanitize=address -o main main.cpp \
   zmytest/test.cpp zlasdtest/test.cpp zlasdtest/container/container.cpp \
   zlasdtest/exercise1a/simpletest.cpp zlasdtest/exercise1a/fulltest.cpp \
   zlasdtest/exercise1b/simpletest.cpp zlasdtest/exercise1b/fulltest.cpp \
