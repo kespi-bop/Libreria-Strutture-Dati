ba8a235412dba18dc3f4ced188f7bd53ddeee1a1
Gianfranco Duminuco <gianfranco.duminuco@gmail.com>
2024-02-29 15:05:12 +0100

    pulizia dei file di progetto pt.1

--- Esercizio1/exercise1/valgrind.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-valgrind --leak-check=full \
-         --show-leak-kinds=all \
-         --track-origins=yes \
-         --verbose \
-         --log-file=valgrind-out.log \
-         ./main
